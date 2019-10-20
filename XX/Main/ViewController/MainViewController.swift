2978346c56857070ae588cb31f4ec199c7101d35
은비 <ebpark9511@gmail.com>
2019-10-20 22:37:00 +0900

    RXCollectionView

--- XX/Main/ViewController/MainViewController.swift
+++ XX/Main/ViewController/MainViewController.swift
@@ -12,14 +12,21 @@ class MainViewController: BaseViewController {
 
     @IBOutlet weak var spendDetailMoveButton: UIButton!
     @IBOutlet weak var addSpendMoveButton: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let HEADER_CELL_NAME = "MainHeaderCell"
+    let CELL_NAME = "MainCell"
     
     var viewModel: MainViewModel?
+    let bag = DisposeBag()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindCollectionView()
+    }
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
-    
-     
 }
 
 extension MainViewController: ViewModelBindableType {
@@ -28,4 +35,84 @@ extension MainViewController: ViewModelBindableType {
         spendDetailMoveButton.rx.action = viewModel.requestSpendDetailMoveAction()
         addSpendMoveButton.rx.action = viewModel.requestAddSpendMoveMoveAction()
     }
+    
+
+    
+    private func bindCollectionView() {
+        let firstCities = ["London", "Vienna", "Lisbon", "Lisbon", "Lisbon"]
+//        let secondCities = ["Paris", "Madrid", "Seoul"]
+        
+        let sections = [
+            SectionModel<String, String>(model: "first section", items: firstCities)
+        ]
+        
+        Observable.just(sections)
+            .bind(to: collectionView.rx.items(dataSource: mainDatasource))
+            .disposed(by: bag)
+    }
+    
+    typealias MainSectionModel = SectionModel<String, String>
+    typealias MainDataSource = RxCollectionViewSectionedReloadDataSource<MainSectionModel>
+    
+    private var mainDatasource: MainDataSource {
+        let configureCell: (CollectionViewSectionedDataSource<MainSectionModel>, UICollectionView, IndexPath, String) -> UICollectionViewCell = { (datasource, collectionView, indexPath,  element) in
+            //            guard
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.CELL_NAME, for: indexPath)
+            //                    as? NameCell else { return UITableViewCell() }
+            
+            //            cell.textLabel?.text = element
+            
+            return cell
+            
+        }
+        
+        let datasource = MainDataSource.init(configureCell: configureCell)
+        
+        datasource.configureSupplementaryView = {(dataSource, collectionView, kind, indexPath) -> UICollectionReusableView in
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: self.HEADER_CELL_NAME, for: indexPath)
+            //                as! HeaderCell
+            //            header.setup()
+            return header
+        }
+        
+        return datasource
+        
+    }
+    
 }
+
+//extension MainViewController: UICollectionViewDelegate {
+//    // 섹션 수 설정
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+//    
+//    // 섹션에 따라 셀 수 정함
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 2
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CELL_NAME, for: indexPath as IndexPath)
+//        return cell
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        print("Cell \(indexPath.row) selected")
+//    }
+//}
+//
+//
+//extension MainViewController: UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+//        if kind == UICollectionView.elementKindSectionHeader {
+//            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader,
+//                                                                             withReuseIdentifier:HEADER_CELL_NAME,
+//                                                                             for: indexPath)
+//            
+//            return headerView
+//        } else {
+//            return UICollectionReusableView()
+//        }
+//    }
+//}
